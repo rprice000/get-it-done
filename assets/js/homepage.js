9a3e09613d74cf57ca4e38051240cb37283cb466
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-09-21 14:54:29 -0500

    resolved list issues problem

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -58,9 +58,10 @@ var displayRepos = function(repos, searchTerm) {
         // format repo name
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-        //create a container for each repo
-        var repoEl = document.createElement("div");
+        //create a link for each repo
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
         //create a span element to hold repository name
         var titleEl = document.createElement("span");
@@ -79,6 +80,10 @@ var displayRepos = function(repos, searchTerm) {
         } else {
             statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+
         //append container to the dom
         repoContainerEl.appendChild(repoEl);
     };
